5ca61be7e017580de737f8f2059f700a479e21a4
Avasiliy1 <77693746+Avasiliy1@users.noreply.github.com>
2021-06-11 11:02:54 +0300

    11.06.2021

---
+++ MetricsManager/MetricsManager/Controllers/CrudController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private readonly ValuesHolder _holder;
+        public CrudController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] WeatherForecast input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Unauthorized(new Error { Message = "Ошибка в передаваемых параметрах!" });
+            }
+            try
+            {
+                _holder.Add(input);
+            }
+            catch (Exception E)
+            {
+                return Unauthorized(new Error { Message = "Не удалось добавить, по причине: " + E.Message });
+            }
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery] DateTime? startDate, DateTime? endDate = null)
+        {
+            return Ok(_holder.Get(startDate ?? DateTime.MinValue, endDate ?? DateTime.MaxValue));
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] WeatherForecast input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Unauthorized(new Error { Message = "Ошибка в передаваемых параметрах!" });
+            }
+            try
+            {
+                _holder.Update(input);
+            }
+            catch (Exception E)
+            {
+                return Unauthorized(new Error { Message = "Не удалось обновить, по причине: " + E.Message });
+            }
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime? startDate, DateTime? endDate = null)
+        {
+            if (startDate == null)
+            {
+                return Unauthorized(new Error { Message = "Не указана StartDate !" });
+            }
+            try
+            {
+                _holder.Delete(startDate.Value, endDate ?? startDate.Value);
+            }
+            catch (Exception E)
+            {
+                return Unauthorized(new Error { Message = "Не удалось удалить, по причине: " + E.Message });
+            }
+            return Ok();
+        }
+    }
+}
